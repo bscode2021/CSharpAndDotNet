579e42eb52601070045da847945028b044e80273
bscode2021 <bscode@hotmail.com>
2022-07-15 21:04:41 +0200

    Raboteno na chapter 02

---
+++ Chapter02/test/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Class1 va = new Class1();
+        }
+    }
+}
